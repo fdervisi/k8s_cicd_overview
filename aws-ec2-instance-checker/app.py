599e59b0c33092fb8837323377864fc26b30305b
fdervisi <fdervisi@googlemail.com>
2023-08-17 09:23:04 +0000

    only public

--- aws-ec2-instance-checker/app.py
+++ aws-ec2-instance-checker/app.py
@@ -65,14 +65,24 @@ def ec2_instances():
                 for tag in instance.tags:
                     if tag['Key'] == 'Name':
                         name = tag['Value']
-            is_imdsv1 = check_imdsv1(instance.id)
+
+                        instance_data = datetime_to_string({
+    'id': instance.id,
+    'name': name,
+    'state': instance.state['Name'],
+    'type': instance.instance_type,
+    'public_ip': instance.public_ip_address
+})
+            opa_response = requests.post(f'{OPA_URL}/v1/data/ec2/match', json={"input": instance_data})
+            is_public_exposed = opa_response.json()['result']
+    
             instances_list.append({
                 'id': instance.id,
                 'name': name,
                 'state': instance.state['Name'],
                 'type': instance.instance_type,
                 'public_ip': instance.public_ip_address,
-                'is_imdsv1': is_imdsv1
+                'is_public_exposed': is_public_exposed
             })
         
         return render_template('instances.html', instances=instances_list)
@@ -85,7 +95,7 @@ def instance(id):
     Returns the details of an EC2 instance with the given ID.
 
     Args:
-        id (str): The ID of the EC2 instance..
+        id (str): The ID of the EC2 instance.
 
     Returns:
         dict: The details of the EC2 instance in JSON format.
@@ -109,70 +119,6 @@ def instance(id):
 
     return jsonify(response)
 
-@app.route('/enable_imdsv2/<id>', methods=['POST'])
-def enable_imdsv2(id):
-    """
-    Enables IMDSv2 for the EC2 instance with the given ID.
-
-    Args:
-        id (str): The ID of the EC2 instance.
-
-    Returns:
-        dict: A JSON object indicating whether the operation was successful.
-    """
-    access_key = session.get('access_key')
-    secret_key = session.get('secret_key')
-    region = session.get('region')
-    
-    aws_session = boto3.Session(
-        aws_access_key_id=access_key,
-        aws_secret_access_key=secret_key,
-        region_name=region
-    )
-    
-    ec2 = aws_session.client('ec2')
-    try:
-        ec2.modify_instance_metadata_options(
-            InstanceId=id,
-            HttpTokens='required',
-            HttpEndpoint='enabled'
-        )
-        return jsonify(success=True)
-    except Exception as e:
-        print(e)
-        return jsonify(success=False)
-
-def check_imdsv1(id):
-    """
-    Checks whether the EC2 instance with the given ID is using IMDSv1.
-
-    Args:
-        id (str): The ID of the EC2 instance.
-
-    Returns:
-        bool: True if the instance is using IMDSv1, False otherwise.
-    """
-    access_key = session.get('access_key')
-    secret_key = session.get('secret_key')
-    region = session.get('region')
-    
-    aws_session = boto3.Session(
-        aws_access_key_id=access_key,
-        aws_secret_access_key=secret_key,
-        region_name=region
-    )
-    
-    ec2 = aws_session.client('ec2')
-    response = ec2.describe_instances(
-        InstanceIds=[
-            id,
-        ]
-    )
-
-    instance_data = datetime_to_string(response['Reservations'][0]['Instances'][0])
-    opa_response = requests.post(f'{OPA_URL}/v1/data/ec2/match', json={"input": instance_data})
-
-    return opa_response.json()['result']
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
